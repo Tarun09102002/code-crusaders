070fea1efdd0825c660fed869619d6fe656c2c9a
Khushal Dhanuka <khushal.dhanuka@spit.ac.in>
2023-02-04 17:51:56 +0530

    added upload notes route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login, Register } from "./pages";
+import { Login, Register, UploadNotes } from "./pages";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/register" element={<Register />}></Route>
+      <Route path="/uploadNotes" element={<UploadNotes />}></Route>
     </Routes>
   );
 }
