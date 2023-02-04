e92325af8041b055acea00657046ae30ca92dca8
Khushal Dhanuka <khushal.dhanuka@spit.ac.in>
2023-02-04 22:08:13 +0530

    library in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login, Register, UploadNotes } from "./pages";
+import { Login, Register, UploadNotes, Library } from "./pages";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <Route path="/login" element={<Login />}></Route>
       <Route path="/register" element={<Register />}></Route>
       <Route path="/uploadNotes" element={<UploadNotes />}></Route>
+      <Route path="/library" element={<Library />}></Route>
     </Routes>
   );
 }
