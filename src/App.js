3fc67e66ce842ff242c6624510be8700a015a7ca
Khushal Dhanuka <khushal.dhanuka@spit.ac.in>
2023-02-05 09:52:22 +0530

    safety commit khushal

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
-	Login,
-	Register,
-	UploadNotes,
-	Library,
-	LandingPage,
-	Chat,
+  Login,
+  Register,
+  UploadNotes,
+  Library,
+  LandingPage,
+  Chat,
 } from "./pages";
 import { Sidebar } from "./components/index";
 
 function App() {
-	return (
-		<>
-			<Sidebar />
-			<Routes>
-				<Route path="/login" element={<Login />}></Route>
-				<Route path="/register" element={<Register />}></Route>
-				<Route path="/uploadNotes" element={<UploadNotes />}></Route>
-				<Route path="/library" element={<Library />}></Route>
-				<Route path="/" element={<LandingPage />}></Route>
-				<Route path="/chat" element={<Chat />}></Route>
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Sidebar />
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/uploadNotes" element={<UploadNotes />}></Route>
+        <Route path="/library" element={<Library />}></Route>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
