f87e2b68447622bbf02d5f27564424b244d7c868
Tarun <tarun.agrawal@spit.ac.in>
2023-02-05 10:31:23 +0530

    safety commit

--- src/pages/WebcamImage.js
+++ src/pages/WebcamImage.js
@@ -1,10 +1,9 @@
-import React from "react";
 import Webcam from "react-webcam";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 const WebcamImage = () => {
-  return <div>Webcam</div>;
+	return <div>Webcam</div>;
 };
 
 export default WebcamImage;
